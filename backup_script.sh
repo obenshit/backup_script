b764b8efc28fdff0b4206b60e1f9646f38dd64b9
obenshit <benshitritori@gmail.com>
2025-02-16 08:35:55 -0500

    backup script file

---
+++ backup_script.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#mkdir "backup" if not exist
+mkdir -p backup
+
+#string with the source filename
+FILE=$1
+
+#check if the user input filename
+if [ -z "$1" ]; then
+	echo "Usage: $0 <filename>"
+	exit 1
+fi
+
+#check if the file exist
+if [ ! -f "$FILE" ]; then
+	echo "Error: File '$FILE' not found!"
+	exit 1
+fi
+
+#make a TIMESTAMP in YYYY-MM-DD_HH-MM-SS Format
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+#Save onlt the file name without path
+FILENAME=$(basename "$FILE")
+
+#Make the file with the new name (with the timestamp)
+BACKUP_FILE="backup/FILE_${TIMESTAMP}_${FILENAME}"
+
+#Make Backup
+cp "$FILE" "$BACKUP_FILE"
+
+
+echo "Backup created: $BACKUP_FILE"
+
+
